0e64d1855cc186f062f40872d317d82d5dd2b4c0
jiyoungnim <cloundyon31@gmail.com>
2025-02-28 16:10:02 +0900

    📝 332. Adding Comments API Routes

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,36 +1,36 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-function handler(req, res) {
-  if (req.method === "GET") {
-    const filePath = path.join(process.cwd(), "data", "comments.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+// function handler(req, res) {
+//   if (req.method === "GET") {
+//     const filePath = path.join(process.cwd(), "data", "comments.json");
+//     const fileData = fs.readFileSync(filePath);
+//     const data = JSON.parse(fileData);
 
-    res.status(200).json({ comments: data });
-  }
+//     res.status(200).json({ comments: data });
+//   }
 
-  if (req.method === "POST") {
-    const email = req.body.email;
-    const name = req.body.name;
+//   if (req.method === "POST") {
+//     const email = req.body.email;
+//     const name = req.body.name;
 
-    const text = req.body.text;
+//     const text = req.body.text;
 
-    const newComment = {
-      id: new Date().toISOString(),
-      email,
-      name,
-      text,
-    };
+//     const newComment = {
+//       id: new Date().toISOString(),
+//       email,
+//       name,
+//       text,
+//     };
 
-    const filePath = path.join(process.cwd(), "data", "comments.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+//     const filePath = path.join(process.cwd(), "data", "comments.json");
+//     const fileData = fs.readFileSync(filePath);
+//     const data = JSON.parse(fileData);
 
-    data.push(newComment);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-    res.status(201).json({ message: "Success!", comments: newComment });
-  }
-}
+//     data.push(newComment);
+//     fs.writeFileSync(filePath, JSON.stringify(data));
+//     res.status(201).json({ message: "Success!", comments: newComment });
+//   }
+// }
 
-export default handler;
+// export default handler;
